65443a8d2a961a673aa959d97e8171b72a622fb5
Richard Carder <rcarder@wellspringadvisors.com>
2016-08-02 23:57:19 -0400

    Slider - trying to use value to change size of statebin squares

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
 var app;
 
-// Declaring our constants
-    var colorRamp = ['#e5f5f9','#2ca25f'];
 
-    var color = d3.scaleLinear()
+// Declaring our constants
+var colorRamp = ['#e5f5f9','#2ca25f'];
+
+var color = d3.scaleLinear()
       .domain([0, 6.5])
       .range(colorRamp);
-
-  // var projection = d3.geo.equirectangular()
-    //  .scale(2000)
-      //.center([-96.03542,41.69553])
-      //.translate([width / 2, height / 2]);
-
-
-      //Will need this to center rects:
-      //squareSize = 19;
-//square
-  //  .attr("x", function(d){ return x(d) - squareSize/2;})
-    //.attr("y", function(d){return y(statusText) - squareSize/2;} )
-   // .attr("width",squareSize)
-    //.attr("height",squareSize)
-
-
-
 
 //d3.queue() enables us to load multiple data files. Following the example below, we make
 //additional .defer() calls with additional data files, and they are returned as results[1],
@@ -35,12 +19,19 @@ var app;
      app.initialize(results[0],results[1]);
    });
 
-app = {
-  data: [],
-  components: [],
+
+  app = {
+    data: [],
+    components: [],
+    options: [],
+    slice: [],
 
   initialize: function (data) {
     app.data = data;
+
+    app.options = {
+        variable: false,
+        };
 
     // Here we create each of the components on our page, storing them in an array
     app.components = [
@@ -48,9 +39,56 @@ app = {
     ];
 
     // Add event listeners and the like here
+       //start slider
+var svg = d3.select("svg"),
+    margin = {right: 50, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height");
+
+var x = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, width])
+    .clamp(true);
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+        .on("start.interrupt", function() { slider.interrupt(); })
+        .on("start drag", function() { hue(x.invert(d3.event.x)); }));
+
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+  .selectAll("text")
+  .data(x.ticks(10))
+  .enter().append("text")
+    .attr("x", x)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d + "°"; });
+
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("r", 9);
+
+function hue(h) {
+  handle.attr("cx", x(h));
+  svg.style("background-color", d3.hsl(h, 0.8, 0.8));
+  app.slice=h;
+  console.log(app.slice);
+ }
 
     // app.resize() will be called anytime the page size is changed
-    d3.select('window').on('resize', app.resize);
+    d3.select(window).on('resize', app.resize);
 
    },
 
@@ -62,6 +100,7 @@ app = {
     app.components.forEach(function (c) { if (c.update) { c.update(); }});
   }
 }
+app.update();
 
 var margin = {
     left: 75,
@@ -108,8 +147,8 @@ chart.svg = d3.select(selector)
 
   chart.sidelength = d3.scaleSqrt()
     .domain([d3.min(app.data, function (d) { return d.pop; }), d3.max(app.data, function (d) { return d.pop; })])
-    .range([25, 60]);
-
+    .range([0, app.slice]);
+  
   chart.update();
 }
     // Data merge:
@@ -145,12 +184,12 @@ Chart.prototype = {
       console.log(app.data)
 
     states.enter().append('rect')
-      .attr('opacity',0)
-      .transition().duration(250)
-      .attr('opacity',1)
-      .attr('class','state')
+      .attr('height',0)
+      .attr('width',0)
       .attr('x', function (d) { return chart.x(d.lon); })
       .attr('y', function (d) { return chart.y(d.lat); })
+      .transition().duration(1000)
+      .attr('class','state')      
       .attr('width', function (d) { return chart.sidelength(d.pop); })
       .attr('height', function (d) { return chart.sidelength(d.pop); })
       .attr('fill',function (d) { return chart.colorScale(d.pop); }) ;

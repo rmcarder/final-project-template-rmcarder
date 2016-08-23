6f9d2f93f8e4391605f1b66a6bbceee14cfa87f8
Richard Carder <rcarder@wellspringadvisors.com>
2016-08-22 23:36:29 -0400

    global color scales

--- script.js
+++ script.js
@@ -38,10 +38,19 @@ var color = d3.scaleLinear()
     app.county=county;
     app.centroids=centroids;
 
-   var max = d3.max(d3.entries(app.leftData));
-   console.log(max);
-   var min = d3.min(d3.entries(app.leftData));
-   console.log(min);
+    //Min and max values for set color scale
+    app.maxMaleLifeEx = d3.max(app.county, function(d) { return d.MaleLifeEx2010;} );
+    app.maxuninsured = d3.max(app.county, function(d) { return d.Uninsured2014;} );
+    app.maxobesity = d3.max(app.county, function(d) { return d.obesity;} );
+    app.maxYPLS = d3.max(app.county, function(d) { return d.YPLS;} );
+    app.maxDaysPoorHealth = d3.max(app.county, function(d) { return d.DaysPoorHealth;} );
+    app.minMaleLifeEx = d3.min(app.county, function(d) { return d.MaleLifeEx2010;} );
+    app.minuninsured = d3.min(app.county, function(d) { return d.Uninsured2014;} );
+    app.minobesity = d3.min(app.county, function(d) { return d.obesity;} );
+    app.minYPLS = d3.min(app.county, function(d) { return d.YPLS;} );
+    app.minDaysPoorHealth = d3.min(app.county, function(d) { return d.DaysPoorHealth;} );
+  
+
 
 
     app.options = {
@@ -211,7 +220,6 @@ var color = d3.scaleLinear()
           d3.select('#slidertext')
           .text(function (d) {return app.options.slider;});
           app.update();
-          chart.update();
         });
 
    d3.select("#slider-black")
@@ -220,16 +228,16 @@ var color = d3.scaleLinear()
           d3.select('#slidertext')
           .text(function (d) {return app.options.slider;});
           app.update();
-          chart.update();
         });
 
    d3.select("#slider-hispanic")
       .on("click", function(){
           app.options.slider="hispanic";
+           d3.select("#slider-hispanic")
+          .classed('btn:active',true);
           d3.select('#slidertext')
           .text(function (d) {return app.options.slider;});
           app.update();
-          chart.update();
         });
 
      d3.select("#slider-asian")
@@ -238,7 +246,6 @@ var color = d3.scaleLinear()
           d3.select('#slidertext')
           .text(function (d) {return app.options.slider;});
           app.update();
-          chart.update();
         });
 
  
@@ -583,13 +590,25 @@ Chart.prototype = {
         //.domain(d3.extent(chart.sum, function (d) { return d[app.options.yvar]; }))
         //.range([d3.interpolateYlOrRd(0.25),d3.interpolateYlOrRd(.75)]);};
 
-chart.data_bins = [d3.min(chart.sum, function (d) { return d[app.options.yvar]; }),
-    ((d3.min(chart.sum, function (d) { return d[app.options.yvar]; })+d3.max(chart.sum, function (d) { return d[app.options.yvar]; }))/2),
-    d3.max(chart.sum, function (d) { return d[app.options.yvar]; })];
 
-if (app.options.yvar==='MaleLifeEx') {chart.color_range = ["#d73027","#ffffbf","#1a9850"];}
-    else {chart.color_range = ["#1a9850","#ffffbf","#d73027"];
-    }
+
+if (app.options.yvar==='MaleLifeEx') {
+    chart.data_bins = [app.minMaleLifeEx,(app.minMaleLifeEx+app.maxMaleLifeEx)/2,app.maxMaleLifeEx];
+  chart.color_range = ["#d73027","#ffffbf","#1a9850"];}
+else if (app.options.yvar==='obesity'){
+      chart.data_bins = [app.minobesity,(app.minobesity+app.maxobesity)/2,app.maxobesity];
+     chart.color_range = ["#1a9850","#ffffbf","#d73027"];}
+else if (app.options.yvar==='DaysPoorHealth'){
+      chart.data_bins = [app.minDaysPoorHealth,(app.minDaysPoorHealth+app.maxDaysPoorHealth)/2,app.maxDaysPoorHealth];
+      chart.color_range = ["#1a9850","#ffffbf","#d73027"];}
+else if (app.options.yvar==='YPLS'){
+      chart.data_bins = [app.minYPLS,(app.minYPLS+app.maxYPLS)/2,app.maxYPLS];
+      chart.color_range = ["#1a9850","#ffffbf","#d73027"];}
+else if (app.options.yvar==='uninsured'){
+      chart.data_bins = [app.minuninsured,(app.minuninsured+app.maxuninsured)/2,app.maxuninsured];
+      chart.color_range = ["#1a9850","#ffffbf","#d73027"];}
+
+    
 chart.colorScale2 = d3.scaleLinear()
         .domain(chart.data_bins)
         .range(chart.color_range);
@@ -612,7 +631,7 @@ chart.colorScale2 = d3.scaleLinear()
       .attr('x', function (d) { return chart.x(d.lon)- 17.5; })
       .attr('y', function (d) { return chart.y(d.lat)- 17.5 - 40; })
     
-      states=states.merge(statesEnter);  
+    states=states.merge(statesEnter);  
 
     states.selectAll('rect')
       .transition().duration(600)    
